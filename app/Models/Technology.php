255ceff9be42c281483f8f3886bc86df47ac5240
AndreaGul <andreagulino99@gmail.com>
2024-02-12 21:23:50 +0100

    feat:model creato con make:model msrRc nello scaffolding

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Technology extends Model
-{
+{   
+    //model creato con il comando  php artisan make:model -msrRc
     use HasFactory;
 }
